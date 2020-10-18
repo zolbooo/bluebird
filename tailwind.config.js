96bd3bd271356e8bade3cff6f1cf23f4365a407a
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-10-18 12:19:17 +0800

    feat: add gray color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   theme: {
     extend: {
       spacing: { 72: '18rem', 84: '21rem', 96: '24rem' },
+      colors: {
+        gray: '#c6c6c6',
+      },
     },
   },
   variants: {},
