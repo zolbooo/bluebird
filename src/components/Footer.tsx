1ffb53028b5a2a55b8f240dfc16f8b10705d62ba
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-10-18 12:31:11 +0800

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import facebookIcon from '@/assets/facebook.svg';
+import instagramIcon from '@/assets/instagram.svg';
+
+function Footer() {
+  return (
+    <footer className="w-full h-32 px-8 bg-black flex flex-row justify-between items-center text-gray">
+      <div className="flex flex-row items-center">
+        <a href="https://www.facebook.com/bluebird0777">
+          <img className="w-5 h-5" alt="Facebook" src={facebookIcon} />
+        </a>
+        <a href="https://www.instagram.com/bluebird_cac" className="ml-2">
+          <img className="w-5 h-5" alt="Instagram" src={instagramIcon} />
+        </a>
+      </div>
+      <div>{/* TODO: Add address */}</div>
+      <span>Copyright</span>
+    </footer>
+  );
+}
+
+export default Footer;
