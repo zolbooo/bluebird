bac5e01e5cd9d26b44fcffd020758ebbdc29a44d
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-10-18 11:59:22 +0800

    chore: disable explicit-module-boundary-types rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,8 @@
 const config = require('@nicepack/eslint-ts/react/next');
+
+config.rules = {
+  ...config.rules,
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
+};
+
 module.exports = config;
